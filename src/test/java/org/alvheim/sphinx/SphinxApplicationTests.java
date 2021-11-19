595c1d92b37c60d17b49fa24d2b69548b895395e
Andrii Koreniuk <epic.andrew@ukr.net>
2021-11-20 00:25:08 +0200

    Initial commit

---
+++ src/test/java/org/alvheim/sphinx/SphinxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alvheim.sphinx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SphinxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
