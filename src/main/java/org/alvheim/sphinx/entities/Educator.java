9dad4e78651181ffa33b9f989b4e6b726d6bff0e
VitaliiZZzz <fake.gutov@gmail.com>
2021-11-23 21:14:27 +0200

    some edits in pom.xml, educator, student, application.properties

--- src/main/java/org/alvheim/sphinx/entities/Educator.java
+++ src/main/java/org/alvheim/sphinx/entities/Educator.java
@@ -6,6 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -19,17 +22,26 @@ public class Educator {
   private Long id;
 
   @Column(name = "first_name")
+  @Size(max = 255)
+  @NotBlank
   private String firstName;
 
   @Column(name = "middle_name")
+  @Size(max = 255)
   private String middleName;
 
   @Column(name = "last_name")
+  @Size(max = 255)
+  @NotBlank
   private String lastName;
 
   @Column(name = "type")
+  @NotBlank
+// TODO protection
   private String type;
 
-  @Column(name = "email")
+  @Column(name = "email", unique = true)
+  @Email
+  @NotBlank
   private String email;
 }
--- src/main/java/org/alvheim/sphinx/entities/Student.java
+++ src/main/java/org/alvheim/sphinx/entities/Student.java
@@ -7,6 +7,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -20,24 +22,32 @@ public class Student {
   private Long id;
 
   @Column(name = "first_name")
+  @Size(max = 255)
+  @NotBlank
   private String firstName;
 
   @Column(name = "middle_name")
+  @Size(max = 255)
   private String middleName;
 
   @Column(name = "last_name")
+  @Size(max = 255)
+  @NotBlank
   private String lastName;
 
-  @Column(name = "username")
+  @Column(name = "username", unique = true)
+  @Size(max = 255)
+  @NotBlank
   private String username;
 
 //  @Column(name = "birthday")
 //  private LocalDate birthday;
 
-  @Column(name = "mobile_number")
+  @Column(name = "mobile_number", unique = true)
+  @NotBlank
   private String mobileNumber;
 
-  @Column(name = "additional_mobile_number")
+  @Column(name = "additional_mobile_number", unique = true)
   private String additionalMobileNumber;
 
 }
